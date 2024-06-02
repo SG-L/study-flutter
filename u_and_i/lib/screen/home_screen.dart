0ba618e63272dcbd60b4eabd629bcb052b9246ce
sgl <sglee8293@naver.com>
2024-06-02 22:50:59 +0900

    u & i > finished

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// 쿠퍼티노 (iOS) 위젯 사용하기 위해 필요
+import 'package:flutter/cupertino.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -24,20 +26,60 @@ class _HomeScreenState extends State<HomeScreen> {
           // 반대축 최대 크기로 늘리기
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _DDay(),
+            _DDay(
+              // 하트 눌렀을 때 실행할 함수 전달
+              onHeartPressed: onHeartPressed,
+              firstDay: firstDay,
+            ),
             _CoupleImage(),
           ],
         ),
       ),
     );
   }
+
+  // 하트 눌렀을 때 실행할 함수
+  void onHeartPressed() {
+    showCupertinoDialog( // 쿠퍼티노 다이얼로그 실행
+      context: context, // 보여줄 다이얼로그 빌드
+      builder: (BuildContext context) {
+        return Align( // 정렬을 지정하는 위젯(자식 위젯)
+          alignment: Alignment.bottomCenter, // 아래 중간으로 정렬
+          child: Container(
+            color: Colors.white, // 배경색 흰색 지정
+            height: 300, // 높이 300 지정
+            child: CupertinoDatePicker( // 날짜 선택하는 다이얼로그
+              mode: CupertinoDatePickerMode.date, // 시간 제외하고 날짜만 선택
+              onDateTimeChanged: (DateTime date) { // 날짜가 변경되면 실행되는 함수
+                setState(() {
+                  firstDay = date;
+                });
+              },
+            ),
+          ),
+        );
+      },
+      barrierDismissible: true, // 외부 탭할 경우 다이얼로그 닫기
+    );
+  }
 }
 
 class _DDay extends StatelessWidget {
+  // 하트 눌렀을 때 실행할 함수
+  final GestureTapCallback onHeartPressed;
+  // 사귀기 시작한 날
+  final DateTime firstDay;
+  
+  _DDay({
+    required this.onHeartPressed, // 상위에서 함수 입력 받기
+    required this.firstDay, // 날짜 변수로 입력 받기
+  });
+  
   @override
   Widget build(BuildContext context) {
     // 테마 불러오기
     final textTheme = Theme.of(context).textTheme;
+    final now = DateTime.now(); // 현재 날짜 시간
 
     return Column(
       children: [
@@ -52,13 +94,14 @@ class _DDay extends StatelessWidget {
           style: textTheme.bodyText1, // bodyText1 스타일 적용
         ),
         Text(
-          '2021.11.23',
+          // DateTime을 년.월.일 형태로 변경
+          '${firstDay.year}.${firstDay.month}.${firstDay.day}',
           style: textTheme.bodyText2, // bodyText2 스타일 적용
         ),
         const SizedBox(height: 16.0),
         IconButton( // 하트 아이콘 버튼
           iconSize: 60.0,
-          onPressed: () {},
+          onPressed: onHeartPressed, // 아이콘 눌렀을 때 실행할 함수
           icon: Icon(
             Icons.favorite,
             color: Colors.red, // 색상 빨강으로 변경
@@ -66,7 +109,7 @@ class _DDay extends StatelessWidget {
         ),
         const SizedBox(height: 16.0),
         Text( // 만난 후 DDay
-          'D+365',
+          'D+${DateTime(now.year, now.month, now.day).difference(firstDay).inDays + 1}',
           style: textTheme.headline2, // headline2 스타일 적용
         ),
       ],
