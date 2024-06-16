20b304b809fd6d3eac44ae995579f5197f5cb2dd
sgl <sglee8293@naver.com>
2024-06-16 19:42:45 +0900

    finished video call

---
+++ video_call/lib/const/agora.dart
@@ -0,0 +1,3 @@
+const APP_ID = 'e16e36db501445749b0c245a06008015';
+const CHANNEL_NAME = 'testchannel';
+const TEMP_TOKEN = '007eJxTYNAQLypNmFZy/X+Qzwphg9X9smxnbOSTp348e/lIyv0DuWkKDKmGZqnGZilJpgaGJiam5iaWSQbJRiamiQZmBgYWBoamoovy0hoCGRlWmtYyMjJAIIjPzVCSWlySnJGYl5eaw8AAANsaIMY=';
